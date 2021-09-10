6e0548a361f02dea255a6f9ff9ece1a9808e3e67
milachal <mila.chalashkanova@ft.com>
2021-09-10 18:50:49 +0300

    Render articles and define basic styling

--- axiosInstance.ts
+++ axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.ft.com/content/",
+  baseURL: "https://api.ft.com/enrichedcontent/",
   timeout: 1000,
-  headers: { "Access-Control-Allow-Origin": "true" },
+  headers: { "Access-Control-Allow-Origin": "true" }
 });
 
 export default instance;
--- components/home-article.tsx
+++ components/home-article.tsx
@@ -1,11 +1,20 @@
 import { ReactElement } from "react";
+import styles from "../styles/HomeArticle.module.scss";
 import { HomeArticleProps } from "../types";
 
-const HomeArticle = ({ title, body }: HomeArticleProps): ReactElement => {
+const HomeArticle = ({
+  meta, title, image, standfirst
+}: HomeArticleProps): ReactElement => {
   return (
-    <div>
-      <div>{title}</div>
-      <div>{body}</div>
+    <div className={styles.homeArticleContainer}>
+      <img
+        className={styles.image}
+        src={image}
+        alt="article"
+      />
+      <div className={styles.meta}>{meta}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.standfirst}>{standfirst}</div>
     </div>
   );
 };
