d81d52d6746fa2bd89cd71dd42d69d5c3b55b6b5
Mila Chalashkanova <53904583+milachal@users.noreply.github.com>
2021-09-16 16:19:49 +0300

    styling for article page (#9)

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,22 +1,30 @@
 import React, { ReactElement } from "react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import parse from "html-react-parser";
 import instance from "../../axiosInstance";
+import styles from "../../styles/article-page.module.scss";
 import { ArticlePageProps } from "../../types";
 import Header from "../../components/header";
 
 const ArticlePage = ({ article }: ArticlePageProps): ReactElement => {
+  console.log(article.body);
   return (
     <div>
       <Header />
-      <div>{article.title}</div>
-      <div>
+      <div className={styles.imageContainer}>
         <img
+          className={styles.image}
           src={article.image}
           alt="article"
         />
       </div>
-      <div>{parse(article.body)}</div>
+      <div className={styles.title}>{article.title}</div>
+      <div className={styles.body}>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: article.body
+          }}
+        />
+      </div>
     </div>
   );
 };
